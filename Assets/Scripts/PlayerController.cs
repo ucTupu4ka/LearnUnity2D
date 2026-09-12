6e82d7e562f2fd4cdf1fd1bcd28324cd2edac736
dmitry <yaucTupu4ka@gmail.com>
2026-09-12 17:31:45 +0300

    Add UI Toolkit assets, including PanelSettings, themes, and layout files; implement PlayerController with score tracking and explosion effects; create ExplosionEffect prefab and UI elements for restart functionality.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,50 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float thrustForce = 1f;
     public float maxSpeed = 10f;
     public GameObject boosterFlame;
+    private float score = 0f;
+    public float scoreMultiplayr = 10f;
+    public UIDocument uiDocument;
+    private float elapsedTime = 0f;
+    public GameObject explosionEffect;
+    private Label scoreText;
+    private Button restartButton;
     Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
+        restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
+        restartButton.style.display = DisplayStyle.None;
+        restartButton.clicked += ReloadScene;
     }
     void Update()
+    {
+        UpdateScore();
+        MovePlayer();
+    }
+
+    void UpdateScore()
+    {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplayr);
+        Debug.Log("Score " + score);
+        scoreText.text = "Score: " + score;
+    }
+
+    void MovePlayer()
     {
         if (Mouse.current.leftButton.isPressed)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-            Debug.Log("Left mouse button pressed" + mousePos);
 
             Vector2 direction = (mousePos - transform.position).normalized;
             transform.up = direction;
@@ -38,9 +64,17 @@ public class PlayerController : MonoBehaviour
         {
             boosterFlame.SetActive(false);
         }
+
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
         Destroy(gameObject);
+        restartButton.style.display = DisplayStyle.Flex;
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
